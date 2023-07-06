cc319fbd3249df648b73400be92f68f33fdbe27d
Angello Gabriel Mansilla Dieguez <127903674+angellomd1309@users.noreply.github.com>
2023-07-06 17:21:29 -0600

    Add files via upload

--- MAPA_DE_GUATEMALA/MAPA_DE_GUATEMALA.ino
+++ MAPA_DE_GUATEMALA/MAPA_DE_GUATEMALA.ino
@@ -43,7 +43,7 @@
 #define ZACAPA 2
 #define BUZZER 3
 
-string Departgen;
+long Departgen;
 
 
 PCF8574 I2C1;
@@ -57,102 +57,166 @@ void setup() {
   
   pinMode(ZACAPA,OUTPUT);
   pinMode(BUZZER,OUTPUT);
+  Serial.begin(9600);
+
+
 
   
 }
 
 void loop() {
+  
   Departgen = random(1,22);
+
+
+
 
   switch(Departgen)
   {
-    case('1'):
+    case(1):
     I2C1.write(ALTAVERAPAZ, HIGH);
+    Serial.println("1");
+    Departgen = 0;
     break;
 
-    case('2'):
+    case(2):
     I2C1.write(BAJAVERAPAZ, HIGH);
+    Serial.println("2");
+    Departgen = 0;
     break;
 
-    case('3'):
+    case(3):
     I2C1.write(CHIMALTENANGO, HIGH);
+     Serial.println("3");
+     Departgen = 0;
     break;
 
-    case('4'):
+    case(4):
     I2C1.write(CHIQUIMULA, HIGH);
+     Serial.println("4");
+     Departgen = 0;
     break;
 
-    case('5'):
+    case(5):
     I2C1.write(GUATEMALA, HIGH);
+     Serial.println("5");
+     Departgen = 0;
     break;
 
-    case('6'):
+    case(6):
     I2C1.write(ELPROGRESO, HIGH);
+     Serial.println("6");
+     Departgen = 0;
     break;
 
-    case('7'):
+    case(7):
     I2C1.write(ESCUINTLA, HIGH);
+     Serial.println("7");
+     Departgen = 0;
     break;
 
-    case('8'):
+    case(8):
     I2C2.write(HUHUETENANGO, HIGH);
+    Serial.print("8");
+    Departgen = 0;
+    if (Serial.available())
+    {
+      long data = Serial.read();
+          if(data == '8')
+      {
+        return Departgen;
+        }
+    
+    }
+    
     break;
 
-    case('9'):
+    case(9):
     I2C2.write(IZABAL, HIGH);
+     Serial.println("9");
+     Departgen = 0;
     break;
 
-    case('10'):
+    case(10):
     I2C2.write(JALAPA, HIGH);
+    Serial.println("10");
+    Departgen = 0;
     break;
 
-    case('11'):
+    case(11):
     I2C2.write(JUTIAPA, HIGH);
+    Serial.println("11");
+    Departgen = 0;
     break;
 
-    case('12'):
+    case(12):
     I2C2.write(PETEN, HIGH);
+    Serial.println("12");
+    Departgen = 0;
     break;
 
-    case('13'):
+    case(13):
     I2C2.write(QUETZALTENANGO, HIGH);
+    Serial.println("13");
+    Departgen = 0;
     break;
 
-    case('14'):
+    case(14):
     I2C2.write(QUICHE, HIGH);
+    Serial.println("14");
+    Departgen = 0;
     break;
 
-    case('15'):
+    case(15):
     I2C3.write(RETALHULEU, HIGH);
+    Serial.println("15");
+    Departgen = 0;
     break;
 
-    case('16'):
+    case(16):
     I2C3.write(SACATEPEQUEZ, HIGH);
+    Serial.println("16");
+    Departgen = 0;
     break;
 
-    case('17'):
+    case(17):
     I2C3.write(SANMARCOS, HIGH);
+    Serial.println("17");
+    Departgen = 0;
     break;
 
-    case('18'):
+    case(18):
     I2C3.write(SANTAROSA, HIGH);
+    Serial.println("18");
+    Departgen = 0;
     break;
 
-    case('19'):
+    case(19):
     I2C3.write(SOLOLA, HIGH);
+    Serial.println("19");
+    Departgen = 0;
     break;
 
-    case('20'):
+    case(20):
     I2C3.write(SUCHITEPEQUEZ, HIGH);
+    Serial.println("20");
+    Departgen = 0;
     break;
 
-    case('21'):
+    case(21):
     I2C3.write(TOTONICAPAN, HIGH);
+    Serial.println("21");
+    Departgen = 0;
     break;
     
-    case('22'):
+    case(22):
     digitalWrite(ZACAPA, HIGH);
-    break;
+
+   Serial.println("22"); 
+       Departgen = 0;
+   break;
+   default:
+   break;
     
   }
 
